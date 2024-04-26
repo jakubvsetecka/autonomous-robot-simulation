d08c3afca8152598d76f047adcbff3c873e2893c
Jakub Vsetecka <jakub01vsetecka@seznam.cz>
2024-04-26 20:54:56 +0200

    feat: :sparkles: Add Simulation class and related files

---
+++ headers/simulation.h
@@ -0,0 +1,31 @@
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+#include "objects.h" // Include the definitions of your game objects
+#include <QFile>
+#include <QGraphicsItem>
+#include <QGraphicsScene>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QList>
+
+class Simulation {
+  public:
+    Simulation();
+    ~Simulation();
+
+    void addObject(GameObject *object);    // Add objects to the simulation
+    void removeObject(GameObject *object); // Remove objects from the simulation
+    void updateObjects();                  // Update all objects in the simulation
+    bool save(const QString &filename);    // Save current state to a file
+    bool load(const QString &filename);    // Load state from a file
+
+  private:
+    QGraphicsScene *scene;
+    QList<GameObject *> objects; // List to hold all objects
+
+    QJsonObject serialize() const;             // Serialize the scene to a JSON object
+    bool deserialize(const QJsonObject &json); // Deserialize the scene from a JSON object
+};
+
+#endif // SIMULATION_H
---
+++ src/simulation.cpp
@@ -0,0 +1,72 @@
+#include "simulation.h"
+#include <QJsonArray> // Add missing include directive
+
+Simulation::Simulation() {
+    scene = new QGraphicsScene();
+}
+
+Simulation::~Simulation() {
+    delete scene;
+    qDeleteAll(objects);
+    objects.clear();
+}
+
+void Simulation::addObject(GameObject *object) {
+    objects.append(object);
+    scene->addItem(object);
+}
+
+void Simulation::removeObject(GameObject *object) {
+    objects.removeOne(object);
+    scene->removeItem(object);
+    delete object;
+}
+
+void Simulation::updateObjects() {
+    for (GameObject *obj : objects) {
+        obj->update();
+    }
+}
+
+bool Simulation::save(const QString &filename) {
+    QFile file(filename);
+    if (!file.open(QIODevice::WriteOnly)) {
+        return false;
+    }
+    QJsonObject simulationState = serialize();
+    QJsonDocument doc(simulationState);
+    file.write(doc.toJson());
+    file.close();
+    return true;
+}
+
+bool Simulation::load(const QString &filename) {
+    QFile file(filename);
+    if (!file.open(QIODevice::ReadOnly)) {
+        return false;
+    }
+    QJsonDocument doc = QJsonDocument::fromJson(file.readAll());
+    bool success = deserialize(doc.object());
+    file.close();
+    return success;
+}
+
+QJsonObject Simulation::serialize() const {
+    QJsonObject root;
+    QJsonArray itemsArray;
+    for (const GameObject *obj : objects) {
+        itemsArray.append(obj->toJson()); // Ensure GameObject has a toJson method
+    }
+    root["items"] = itemsArray;
+    return root;
+}
+
+bool Simulation::deserialize(const QJsonObject &json) {
+    QJsonArray itemsArray = json["items"].toArray();
+    for (const QJsonValue &value : itemsArray) {
+        QJsonObject objJson = value.toObject();
+        GameObject *obj = GameObject::fromJson(objJson); // Static method to create objects from JSON
+        addObject(obj);
+    }
+    return true;
+}
