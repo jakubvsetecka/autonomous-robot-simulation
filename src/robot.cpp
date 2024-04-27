be6eca0074e809a9eeaa0d427882b381500900c7
Tomáš Hobza <xhobza03@vutbr.cz>
2024-04-27 23:10:18 +0200

    feat: :sparkles: autonomous robot line

--- src/robot.cpp
+++ src/robot.cpp
@@ -5,17 +5,23 @@
  */
 
 #include "robot.h"
+#include <iostream>
 
 Robot::Robot(QGraphicsItem *parent, QPointF position, QPointF dimension, double angle, double velocity)
     : GameObject(parent, position, dimension, angle, velocity) {}
 
-void Robot::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
+void Robot::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
     QBrush brush(Qt::CrossPattern);
     painter->setBrush(brush);
     painter->drawEllipse(boundingRect());
+    qreal x_center = dimension.x() / 2;
+    qreal y_center = dimension.y() / 2;
+    painter->drawLine(x_center, y_center, (angleDir.getX() * x_center) + x_center, (angleDir.getY() * y_center) + y_center);
 }
 
-void Robot::update() {
+void Robot::update()
+{
     moveBy(angleDir.getX(), angleDir.getY());
     setRotation(angleDir.angle);
 }
