a1da47a1b8ea1ed99346ad2b4dd0e2564415521c
Tomáš Hobza <xhobza03@vutbr.cz>
2024-04-28 22:21:27 +0200

    chore: :art: #2 fomatting

--- src/simulationengine.cpp
+++ src/simulationengine.cpp
@@ -4,7 +4,8 @@
 qreal SimulationEngine::timeConstant = 0.5;
 
 SimulationEngine::SimulationEngine(QObject *parent, int fps, int simulationSpeed)
-    : QGraphicsScene(parent) {
+    : QGraphicsScene(parent)
+{
     // Set the frame rate and simulation speed
     this->fps = fps;
     this->simulationSpeed = simulationSpeed;
@@ -21,45 +22,53 @@ SimulationEngine::SimulationEngine(QObject *parent, int fps, int simulationSpeed
     setControlledRobot(robutek);
 
     // Add an autonomous robot
-    AutoRobot *samorobutek = new AutoRobot(nullptr, 10, Robot::RotationDirection::Right, 7, 1);
+    AutoRobot *samorobutek = new AutoRobot(nullptr, 30, Robot::RotationDirection::Right, 7, 1);
     samorobutek->setPos(150, 150);
     addItem(samorobutek);
 }
 
 SimulationEngine::~SimulationEngine() {}
 
-int SimulationEngine::getFPS() {
+int SimulationEngine::getFPS()
+{
     return fps;
 }
 
-int SimulationEngine::getFrameTime() {
+int SimulationEngine::getFrameTime()
+{
     return 1000 / fps;
 }
 
-qreal SimulationEngine::getSimulationSpeed() {
+qreal SimulationEngine::getSimulationSpeed()
+{
     return simulationSpeed;
 }
 
-Robot *SimulationEngine::getControlledRobot() {
+Robot *SimulationEngine::getControlledRobot()
+{
     return controlledRobot;
 }
 
-void SimulationEngine::setControlledRobot(Robot *robot) {
+void SimulationEngine::setControlledRobot(Robot *robot)
+{
     controlledRobot = robot;
     addItem(controlledRobot);
 }
 
-void SimulationEngine::addObstacle() {
+void SimulationEngine::addObstacle()
+{
     Obstacle *obstacle = new Obstacle();
     addItem(obstacle);
 }
 
-void SimulationEngine::addAutoRobot() {
+void SimulationEngine::addAutoRobot()
+{
     AutoRobot *autoRobot = new AutoRobot(nullptr, 10, Robot::RotationDirection::Right, 7, 1);
     addItem(autoRobot);
 }
 
-void SimulationEngine::addControlledRobot() {
+void SimulationEngine::addControlledRobot()
+{
     Robot *robot = new Robot();
     setControlledRobot(robot);
 }
